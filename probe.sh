bf134242c9e39a222507f001cdc9a8c4d0eda6a9
Rasmus Leißner <rleissner@irhh.de>
2015-06-18 15:47:22 +0200

    seperated sign and probe, longer probe time

---
+++ probe.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+GPG=$HOME/Develop/flush-reload/build_gpg/gnupg-1.4.12/bin/gpg
+ADDR=addr/na_ubu14.txt
+MESSAGE=message.txt
+OUT=out.txt
+CYCLES=$1
+
+bin/probe ${GPG} ${ADDR} ${OUT} ${CYCLES} &
+PROBE_PID=$!
+
+#sleep 0.01
+#(echo "GPG start"; ${GPG} --yes --sign ${MESSAGE}; echo "GPG end") &
+#GPG_PID=$!
+
+trap "echo 'Received signal'; kill -TERM ${PROBE_PID}" \
+    SIGINT SIGQUIT
+
+wait ${PROBE_PID}
+#wait ${GPG_PID}
+
+python graph.py &
+GRAPH_PID=$!
+
+trap "echo 'Killing python'; kill -TERM ${GRAPH_PID}" SIGINT SIGQUIT
+wait ${GRAPH_PID}
---
+++ sign.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+GPG=$HOME/Develop/flush-reload/build_gpg/gnupg-1.4.12/bin/gpg
+MESSAGE=message.txt
+OUT=out.txt
+
+
+i="0"
+
+
+
+echo "start 100x GPG sign"
+
+while [ $i -lt 100 ]
+do
+	echo $i
+	${GPG} --yes --sign ${MESSAGE}
+	sleep 0.1
+
+i=$[$i+1]
+done
+
+echo "GPG stop"
+
+
+
+exit 0
