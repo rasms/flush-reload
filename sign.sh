ced4df805b888cc8f448d61b4c921b3a0ecfeb49
rasmus <rleissner@irhh.de>
2015-07-16 14:35:49 +0200

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -3,19 +3,19 @@
 GPG=$HOME/flush-reload-master/build_gpg/gnupg-1.4.12/bin/gpg
 MESSAGE=message.txt
 OUT=out.txt
+CYCLES=$1
 
 
 i="0"
 
 
 
-echo "start 100x GPG sign"
+echo "start GPG sign"
 
-while [ $i -lt 100 ]
+while [ $i -lt ${CYCLES} ]
 do
 	echo $i
 	${GPG} --yes --sign ${MESSAGE}
-	sleep 0.1
 
 i=$[$i+1]
 done
